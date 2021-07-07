b9d483eb26f00b5c9f812e16e0f719295b54ff0d
YudachiK2 <73721400+YudachiK2@users.noreply.github.com>
2021-07-07 18:01:57 +0900

    feat: commit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 'use strict'
 import express from 'express'
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.header('Content-Type', 'application/json; charset=utf-8')
-    res.sendStatus(400)
-    res.send({})
+    console.log(req.body)
 })
 
 const listener = app.listen(PORT, () => console.log(`Your app is listening on port ${PORT}`))
