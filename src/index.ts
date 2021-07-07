4421c32b5a3419a0587e4e0c64fe55bee8f6161f
YudachiK2 <73721400+YudachiK2@users.noreply.github.com>
2021-07-07 20:15:00 +0900

    test: commit

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,16 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.post('/', (req, res) => {
-    console.log(req.headers['content-type'], req.body)
-    res.sendStatus(400)
+    const type = req.headers['content-type']
+
+    if (type !== 'application/json' && type !== 'application/x-www-form-urlencoded') {
+        res.sendStatus(400)
+    } else {
+        console.log(req)
+    }
 })
 
 const listener = app.listen(PORT, () => console.log(`Your app is listening on port ${PORT}`))
