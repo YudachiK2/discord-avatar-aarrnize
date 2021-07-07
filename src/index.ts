2f2484df9b0b9df7204c0a4f34eaf9f0d9061f46
YudachiK2 <73721400+YudachiK2@users.noreply.github.com>
2021-07-07 22:38:30 +0900

    test: commit

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@
 import express from 'express'
 import axios from 'axios'
 import fileType from 'file-type'
-
-const gifExtractFrames = require('gif-extract-frames')
+import canvas from 'canvas'
 
 const PORT = process.env.PORT || 3000
 
@@ -40,9 +39,56 @@ app.post('/', async (req, res) => {
             return
         }
 
-        console.log(await gifExtractFrames('https://i.imgur.com/2T7glHi.gif'))
+        Promise.all([
+            '9l03drB.png',
+            '3FYaLnV.png',
+            'Yr7oux9.png',
+            'Yzs2oRa.png',
+            'j87lmeC.png',
+            'p4GOTwS.png',
+            'J71JiaT.png',
+            '5qVvm93.png',
+            'Pslq7Fg.png',
+            'pWZveSy.png',
+            'uSFN4Ds.png',
+            'u44vDX9.png',
+            'vVI4At0.png',
+            'TyrbySp.png',
+            '6xVH4Il.png',
+            'Ogphmd6.png',
+            'Etk577o.png',
+            'gTxgdRL.png',
+            'K1HNupK.png',
+            '4cMBBjt.png',
+            'gcZzO8q.png',
+            'fze1UWK.png',
+            'DKudT4R.png',
+            'OxNZRlD.png',
+            '5weN7lB.png'
+        ].map(v => new Promise((reject, resolve) => {
+            axios({
+                url: `https://i.imgur.com/${v}`,
+                method: 'get',
+                responseType: 'arraybuffer'
+            }).then(res => {
+                fileType.fromBuffer(res.data)
+                    .then(fileInfo => {
+                        if (fileInfo === void 0) {
+                            throw new TypeError()
+                        }
 
-        res.sendStatus(200)
+                        const img = new canvas.Image
+                        img.onload = () => resolve(img)
+                        img.onerror = e => reject(e)
+                        img.src = `data:${fileInfo.mime};base64,${res.data.toString('base64')}`
+                    })
+                    .catch(e => reject(e))
+            }).catch(e => reject(e))
+        })))
+            .then(imgs => {
+                console.log(imgs)
+            })
+            .catch(e => console.error(e))
     } catch (e) {
         console.error(e)
         res.sendStatus(400)
