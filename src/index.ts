18305534f3bbeed03147c5faead9e97b2ad2af93
YudachiK2 <73721400+YudachiK2@users.noreply.github.com>
2021-07-07 17:07:36 +0900

    feat: commit

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.post('/', (req, res) => {
-    console.log(req, res)
+    res.setHeader('Access-Control-Allow-Origin', 'https://zippy-childlike-anise.glitch.me')
 })
 
 const listener = app.listen(PORT, () => console.log(`Your app is listening on port ${PORT}`))
