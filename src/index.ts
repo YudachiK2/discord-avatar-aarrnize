72427c59838d50aa9c5b6dbbee54b198860065cd
YudachiK2 <73721400+YudachiK2@users.noreply.github.com>
2021-07-07 21:42:51 +0900

    fix: commit

--- src/index.ts
+++ src/index.ts
@@ -23,23 +23,19 @@ app.post('/', async (req, res) => {
         res.sendStatus(400)
         return
     }
+    try {
+        const imgRes = await axios({
+            url: image,
+            method: 'get',
+            responseType: 'arraybuffer'
+        })
 
-    const imgRes = await axios({
-        url: image,
-        method: 'get',
-        responseType: 'arraybuffer'
-    })
+        console.log(imgRes)
 
-    console.log(imgRes)
-
-    res.sendStatus(200)
-    /*console.log(typeof (await axios({
-        url: image,
-        method: 'get',
-        responseType: 'arraybuffer'
-    })).data)
-
-    res.sendStatus(200)*/
+        res.sendStatus(200)
+    } catch {
+        res.sendStatus(400)
+    }
 })
 
 const listener = app.listen(PORT, () => console.log(`Your app is listening on port ${PORT}`))
