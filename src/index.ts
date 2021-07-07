a43d8b91c508e9109de9d0f07f6489463a9e59cd
YudachiK2 <73721400+YudachiK2@users.noreply.github.com>
2021-07-07 17:02:05 +0900

    test: Test commit

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,8 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.post('/', (req, res) => {
+    console.log(req, res)
+})
+
 const listener = app.listen(PORT, () => console.log(`Your app is listening on port ${PORT}`))
