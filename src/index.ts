8150008856a0725f1bd4d846416c81fae6c1ff64
YudachiK2 <73721400+YudachiK2@users.noreply.github.com>
2021-07-07 20:30:21 +0900

    feat: commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 'use strict'
 import express from 'express'
+import axios from 'axios'
 
 const PORT = process.env.PORT || 3000
 
@@ -7,15 +8,24 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     const type = req.headers['content-type']
 
     if (type !== 'application/json') {
         res.sendStatus(400)
-    } else {
-        console.log(req)
-        res.sendStatus(200)
+        return
     }
+
+    const [image]: [string] = req.body
+
+    if (typeof image !== 'string') {
+        res.sendStatus(400)
+        return
+    }
+
+    console.log(await axios.get(image))
+
+    res.sendStatus(200)
 })
 
 const listener = app.listen(PORT, () => console.log(`Your app is listening on port ${PORT}`))
