d55edd091737bdd34f07dba5d7cfe240c2897268
YudachiK2 <73721400+YudachiK2@users.noreply.github.com>
2021-07-07 20:21:16 +0900

    fix: commit

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,16 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.post('/', (req, res) => {
     const type = req.headers['content-type']
 
-    if (type !== 'application/json' && type !== 'application/x-www-form-urlencoded') {
+    if (type !== 'application/json') {
         res.sendStatus(400)
     } else {
         console.log(req)
+        res.sendStatus(200)
     }
 })
 
